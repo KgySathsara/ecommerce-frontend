8df050ad6368c51b1edb2d2657664942fa71fba8
Akith Nimpura <mpakithn@gmail.com>
2024-08-04 23:40:01 +0530

    Contactus and About us pages created

--- src/pages/User/Aboutus.js
+++ src/pages/User/Aboutus.js
@@ -1,13 +1,36 @@
-import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
+import React from 'react';
+import Navbar from '../../components/Navbar/Navbar';
+import { Card, Row, Col } from 'antd';
+import './user.css';
+import picture from '../../assets/chicken.jpg';
+
+const { Meta } = Card;
 
 const Aboutus = () => {
   return (
-    <div>
+    <section>
       <Navbar />
-      AboutUs
-    </div>
-  )
+      <Card
+        hoverable
+        style={{
+          width: 500,
+        }}
+      >
+        <Row>
+          <Col flex="100px">
+            <img
+              alt="example"
+              src={ picture }
+              className="aboutus-image"
+            />
+          </Col>
+          <Col flex="auto" className="aboutus-details">
+            <Meta title="Prisco Chicken Product" description="Prisco chicken is high quality product that is..." paragraph="Read More.." />
+          </Col>
+        </Row>
+      </Card>
+    </section>
+  );
 }
 
-export default Aboutus
+export default Aboutus;
